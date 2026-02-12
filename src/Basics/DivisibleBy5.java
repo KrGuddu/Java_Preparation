37a8a780d86f7a597d29cdef83f9af66e5ea264e
KrGuddu <krguddu684@gmail.com>
2026-02-12 16:53:56 +0530

    Code added

---
+++ src/Basics/DivisibleBy5.java
@@ -0,0 +1,19 @@
+// Take positive integer input and tell if it is divisible by 5 or not.
+
+package Basics;
+import java.util.Scanner;
+
+public class DivisibleBy5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        if (n%5 == 0) {
+            System.out.println("Yes, It is divisible by 5");
+        }else {
+            System.out.println("Not divisible by 5");
+        }
+    }
+}
