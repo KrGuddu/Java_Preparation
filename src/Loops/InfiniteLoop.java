28d004a9c55f8c1cf7bd7c27c09204ff8f070d5f
KrGuddu <krguddu684@gmail.com>
2026-02-15 23:13:49 +0530

    Infinite loop

---
+++ src/Loops/InfiniteLoop.java
@@ -0,0 +1,15 @@
+// Note: Not recommended to use Infinite Loop. and also we get an error to use this.
+package Loops;
+
+public class InfiniteLoop {
+    public static void main(String[] args) {
+//        for (int i = 1; i <= 10 ; i--) {        // Infinite Loop ==> Not recommended to use this.
+//            System.out.println(i);
+//        }
+
+        //or,
+//        while (1<2){    // Jabtak 1 lesser hai 2 se tabtak loop ko chalate rho. ==>> also not recommended.
+//            System.out.println("Infinite Loop");
+//        }
+    }
+}
