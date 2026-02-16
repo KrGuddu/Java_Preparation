35c0858c254bc884ce5c32fc152266b2f2875aee
KrGuddu <krguddu684@gmail.com>
2026-02-16 22:05:44 +0530

    Alphabet Square

---
+++ src/Pattern_Printing/AlphabetSquare.java
@@ -0,0 +1,17 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class AlphabetSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=n; j++) {
+                System.out.print( (char)(j+64) + " " );   //count me 64 add krne par Uppercase alphabet ka ascii value aa jayegi then usse typecast kar dene par unn savi value ki alphabet aa jayegi
+            }
+            System.out.println();
+        }
+    }
+}
