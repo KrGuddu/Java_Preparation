f2a8f9c47accf4a58a6acbe2930e0db7a386a27e
KrGuddu <krguddu684@gmail.com>
2026-02-19 18:26:21 +0530

    StarTriangleVerticallyFlipped

---
+++ src/Pattern_Printing/StarTriangleVerticallyFlipped.java
@@ -0,0 +1,20 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class StarTriangleVerticallyFlipped {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(i+j>n){
+                    System.out.print("* ");
+                }else {         // i+j <= n
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
