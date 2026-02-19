f2088aac9f574ca227042a2b3f61e43f49322086
KrGuddu <krguddu684@gmail.com>
2026-02-19 18:23:50 +0530

    Pyramid_2ndMethod

---
+++ src/Pattern_Printing/Pyramid_2ndMethod.java
@@ -0,0 +1,21 @@
+//more modulus according to 1st method
+
+package Pattern_Printing;
+import java.util.Scanner;
+
+public class Pyramid_2ndMethod {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=n-i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <=2*i-1; j++) {       //if n=1 then j=1 so *  //if n=2 then j=3 so ***   //if n=3 then j=5 so *****   //if n=4 then j=1 so *******
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
