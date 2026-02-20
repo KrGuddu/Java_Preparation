4aa45b7802f9327a7986bda53883af1658626e51
KrGuddu <krguddu684@gmail.com>
2026-02-20 18:35:15 +0530

    PermutationAndCombination_WithoutMethod

---
+++ src/Methods/PermutationAndCombination_WithoutMethod.java
@@ -0,0 +1,26 @@
+package Methods;
+import java.util.Scanner;
+
+public class PermutationAndCombination_WithoutMethod {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+
+        int nFact =1;
+        for (int i = 1; i<=n ; i++) {
+            nFact *= i;
+        }
+        int rFact =1;
+        for (int i = 1; i<=r ; i++) {
+            rFact *= i;
+        }
+        int nrFact =1;
+        for (int i = 1; i<=n-r ; i++) {
+            nrFact *= i;
+        }
+
+        int ncr = nFact/(rFact*nrFact);
+        System.out.println(ncr);
+    }
+}
