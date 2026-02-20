3c23eb03be7cf5bbd6b5972c83256f5b817561b1
KrGuddu <krguddu684@gmail.com>
2026-02-20 18:32:30 +0530

    MaxOfThreeBuiltIn

---
+++ src/Methods/MaxOfFourBuiltIn.java
@@ -0,0 +1,17 @@
+// Que: Find the maximum of four number using built-in function
+
+package Methods;
+
+import java.util.Scanner;
+
+public class MaxOfFourBuiltIn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        System.out.println(Math.max(Math.max(a,b), Math.max(c,d)));
+    }
+}
