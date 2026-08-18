f93daad4cb0eec9e12ed215de30e1448141b300e
KrGuddu <krguddu684@gmail.com>
2026-08-18 20:04:50 +0530

    chore: Single_Among_Doubles_In_a_Sorted

---
+++ src/BinarySearch/Single_Among_Doubles_In_a_Sorted.java
@@ -0,0 +1,15 @@
+package BinarySearch;
+
+public class Single_Among_Doubles_In_a_Sorted {
+}
+
+
+/*
+Iss type ka question hamesa odd number of elements me honge qki sare elements double honge bass ek element single honge
+Solution steps:
+1st: consider the low, high and middle element
+2nd: compare middle element to the own left and right elements
+3rd: if the element is same as middle element the go where the number of elements is odd.
+
+jab element compared karege to middle and compared element(jo ki same element hai) neglected ho jayega jo ki pair me hote hai.
+* */
