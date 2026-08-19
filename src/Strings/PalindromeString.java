90abe45368a8886ae00223a2765e5b6191ce6404
KrGuddu <krguddu684@gmail.com>
2026-08-19 20:40:49 +0530

    feat: Add String questions on Palindrome

---
+++ src/Strings/PalindromeString.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class PalindromeString {
+
+}
+
+
+/*
+Rule for charAt
+
+Galat ❌	        Sahi ✅
+s.charAt[i]	        s.charAt(i)
+s.length	        s.length()
+s.toLowerCase	    s.toLowerCase()
+
+
+Important: Java mein String ko array ki tarah s[i] se bhi access nahi kar sakte. Character nikalne ke liye s.charAt(i) use karna hota hai.
+* */
