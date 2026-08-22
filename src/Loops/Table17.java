74227e1a5ee07d2177d784a8e5405fdf0c83f6a8
KrGuddu <krguddu684@gmail.com>
2026-08-22 21:13:21 +0530

    refactor: solution update

--- src/Loops/Table17.java
+++ src/Loops/Table17.java
@@ -10,12 +10,12 @@ public class Table17 {
 //            }
 //        }
 
-        //Or, more Efficient way =>b/q ye loop only 10 times chalega/Iteration.   //isme loop 187 [b/q last time 170 par chalega phir increament hokar 170+17=187 par condition false hogi] par break hogi.
+        //Or, more Efficient way =>b/q ye loop only 10 times chalega/Iteration.   //isme loop 11th par break hogi, jispar value 187 hogi[b/q last time 170 par chalega phir increament hokar 170+17=187 par condition false hogi].
 //        for (int i=17; i<=170; i=i+17){
 //            System.out.println(i);
 //        }
         //or
-        for (int i=1; i<=10; i=i+1){         // Not write i++ here
+        for (int i=1; i<=10; i=i+1){         //Also write i++ not issue.          // Not write i++ here
             System.out.println(i*17);
         }
     }
