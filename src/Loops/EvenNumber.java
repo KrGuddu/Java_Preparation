b38a1ebc8b27c49f512fe2f26dafca0f0ddf0ad9
KrGuddu <krguddu684@gmail.com>
2026-08-22 21:13:13 +0530

    refactor: solution update

--- src/Loops/EvenNumber.java
+++ src/Loops/EvenNumber.java
@@ -4,8 +4,8 @@ package Loops;
 public class EvenNumber {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
-            if (i%2 == 0) {
+        for (int i = 1; i <= 100; i++) {      //Solution step1: fetch all numbers from 1 to 100     //step2. then apply even number condition and print it.
+            if (i%2 == 0) {                    //for odd number: i%2 != 0
                 System.out.println(i);
             }
         }
