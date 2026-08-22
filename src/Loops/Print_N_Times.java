c5233d28520fc2ee78cfea0b7b715bb400d2fe14
KrGuddu <krguddu684@gmail.com>
2026-08-22 21:12:55 +0530

    refactor: solution update

--- src/Loops/Print_N_Times.java
+++ src/Loops/Print_N_Times.java
@@ -8,21 +8,31 @@ public class Print_N_Times {
         int n = sc.nextInt();
 
         //Que: Print number ‘n’ times. Take ‘n’ input from user
-        // ye Infinite loop tak chalegi.
-        for (n=0; n<=n; n++){
-            System.out.println(n);
-        }
+        // ye Infinite loop tak chalegi.            ==>> Not Recommended
+//        for (n=0; n<=n; n++){
+//            System.out.println(n);
+//        }
 
         // ye Infinite loop tak nhi chalegi, user jitna input dega wahi tak chalega.
-        int num;
-        for (num=0; num<=n; num++){
-            System.out.println(num);
-        }
+//        int num;
+//        for (num=0; num<=n; num++){
+//            System.out.println(num);
+//        }
 
         //Que: Print ____Yashika_____ ‘n’ times. Take ‘n’ input from user.
         int Yashika;            // datatype int leni hai qki input value integer datatype hogi. jo ki user dega
         for (Yashika=0; Yashika<=n; Yashika++){
-            System.out.println("Yashika");         //Yaha datatype String isliye liya gya hai qki Yashika ko string ki format me print krani hai         // user jitna input dega utni bar Yashika print hogi
+            System.out.println("Yashika");         //Yaha datatype String isliye liya gya hai qki Yashika ko string literal ki format me print karni hai         // user jitna input dega utni bar Yashika print hogi
+        }
+
+        //or
+        for(int i=1; i<=n; i++){
+            System.out.println("Yashika");
+        }
+
+        //Print X n times
+        for (int i = 1; i <= n; i++) {
+            System.out.println("X");
         }
     }
 }
