64348fd5b43411c49b9df261e105bfd7933e7428
KrGuddu <krguddu684@gmail.com>
2026-08-26 20:46:33 +0530

    refactor: Optimize solution

--- src/Loops/Ascii.java
+++ src/Loops/Ascii.java
@@ -4,10 +4,24 @@ package Loops;
 
 public class Ascii {
     public static void main(String[] args) {
-        for (int i = 65; i <=90 ; i++) {        // (int i = 97; i <=122; i++) => for Lowercase letters a-z      // (int i = 48; i <=57; i++) =>for digits 0-9
-//            System.out.println(i);    //print 65 to 90
-//            System.out.println((char) i);    // print capital alphabet A-Z
+        for (int i = 0; i <=127 ; i++){        // (int i = 97; i <=122; i++) => for Lowercase letters a-z      // (int i = 48; i <=57; i++) =>for digits 0-9
+//          System.out.println(i);    //print 65 to 90
+//          System.out.println((char) i);    // print capital alphabet A-Z
             System.out.println((char) i + " : " + i);    //print A-Z corresponding their number 65-90
         }
+
+//        for (int i = 65; i <=90 ; i++){               //for all ASCII values.
+//            System.out.println((char)i + " " + i);
+//        }
     }
 }
+
+
+/*
+0-9: 48-57
+A-Z: 65-90
+a-z: 97-122
+
+?: 63
+@: 64
+* */
--- src/Loops/PrimeNumber.java
+++ src/Loops/PrimeNumber.java
@@ -7,19 +7,26 @@ import java.util.Scanner;
 public class PrimeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter Number: ");
         int n = sc.nextInt();
         boolean flag = true;    // true means prime
+
         for (int i = 2; i <= n-1 ; i++) {
-            if (n%2==0){
+            if (n%i==0){
 //                System.out.println("Composite");
-                flag = false;   // false means composite
+                flag = false;   // false means composite/not prime                  //"flag = false" or "flag = true" is checking factor.
                 break;
             }
         }
-        if (n==1) System.out.println("Neither prime nor composite");
+        if (n<=1) System.out.println("Neither prime nor composite");
         else if (flag == false) System.out.println("Composite");
         else System.out.println("Prime");
     }
 }
+
+
+/*
+Prime vs Composite: Difference sirf interpretation ka hai:
+Factor mil gaya → Composite / Not Prime
+Factor nahi mila → Prime / Not Composite
+* */
