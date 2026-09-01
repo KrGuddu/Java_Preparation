586a72c93cbfcb490cd55cb5f880c81d9a0a1807
KrGuddu <krguddu684@gmail.com>
2026-09-01 20:43:36 +0530

    feat: Recursion questions on PreInPost

---
+++ src/Recursion/PreInPost.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class PreInPost {
+    public static void main(String[] args) {
+        pip(3);
+    }
+    public static void pip(int n){
+        if(n==0) return;        //return sirf particular/current statement ko hi terminate karta hai, baki sare code ka kam karta hai.
+        System.out.print(n + " ");  //pre: call se pahle kam
+        pip(n-1);
+        System.out.print(n + " ");  //In: do call ke bich ka kam
+        pip(n-1);
+        System.out.print(n + " "); //post: call ke baad ka kam
+    }
+}
