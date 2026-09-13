7ebfa243b030f4cc34b4f8c7c690972f8d1117ed
KrGuddu <krguddu684@gmail.com>
2026-09-13 19:44:49 +0530

    refactor: Logic updated

--- src/Array/OutputInput_Que.java
+++ src/Array/OutputInput_Que.java
@@ -7,10 +7,10 @@ public class OutputInput_Que {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[5];
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {       //Always use arr.length not hard coded value
             arr[i] = sc.nextInt();
-            arr[i] *= 2;
-            System.out.print(arr[i] + " ");     //Or, 2*arr[i]
+            arr[i] *= 2;                            //Array gets modified   =>> Recommended
+            System.out.print(arr[i] + " ");        //Or, 2*arr[i] but in this version the original array values remain unchanged.   //Array not modified    =>>Not Recommended
         }
     }
 }
--- src/Array/OutputInput_QuePart2.java
+++ src/Array/OutputInput_QuePart2.java
@@ -1,7 +1,6 @@
 //Que: Take n number of user i/p and print the array in 2x or 2 multiply of array.
 
 package Array;
-
 import java.util.Scanner;
 
 public class OutputInput_QuePart2 {
@@ -9,10 +8,19 @@ public class OutputInput_QuePart2 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
             arr[i] *= 2;
             System.out.print(arr[i] + " ");
         }
     }
 }
+
+
+
+/*If Input is:
+5
+1 2 3 4 5
+
+Then Output is: 2 4 6 8 10
+ */
