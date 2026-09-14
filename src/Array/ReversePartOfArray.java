57b6cddcc63503526aa0dcd065c319b173813d3c
KrGuddu <krguddu684@gmail.com>
2026-09-14 21:20:33 +0530

    refactor: Update logic of the code

--- src/Array/ReversePartOfArray.java
+++ src/Array/ReversePartOfArray.java
@@ -9,7 +9,7 @@ public class ReversePartOfArray {
     public static void main(String[] args) {
         int[] arr = {0,10,20,30,40,-50,60,-70,80,90};
         int n = arr.length;
-        int i=2, j=6;     //or use, j=n-1-3 => j=n-4   ==> kha se kha tak matlab jaha se krna hai usse i assume kare and jaha tak krna hai usse j assume kare.
+        int i=2, j=6;               //Also use for this array, j=n-1-3 => j=n-4   ==> kha se kha tak matlab jaha se krna hai usse i assume kare and jaha tak krna hai usse j assume kare.
 
         while (i<j){
             int temp = arr[i];
--- src/Array/RotateArray.java
+++ src/Array/RotateArray.java
@@ -6,13 +6,13 @@ import java.util.*;
 public class RotateArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        int d = 2;      //d is the number of rotation
+        int d = 2;      // d = number of positions to rotate
         rotateArr(arr, d);
         System.out.println(Arrays.toString(arr));
     }
     static void rotateArr(int arr[], int d) {
         int n= arr.length;
-        d = d%n;    // Agar d>n ho jaaye to error ya wrong output nahi aayega.  //Ex: n=5, d=7 then 7%5 = 2  ==> Matlab actually 2 hi rotate karna hai.
+        d = d%n;    // Agar d>n ho jaaye to error ya wrong output nahi aayega.  //Ex: n=5, d=7 then 7%5 = 2  ==> Matlab actually 2 hi rotate karna hai.     //19 % 5 = 4 means 4 rotation hogi => agar n=5, d=19 hai to har 5 rotation par original array ayega and baki 4 actual rotation hoge jisse 19 bar rotate karna nhi parta hai.
 
         reverse(arr, 0, d-1);
         reverse(arr, d, n-1);
@@ -30,3 +30,37 @@ public class RotateArray {
 }
 
 //So, Time complexity = O(n), Space Complexity = O(1) (No extra array)
+/* Ek simple rule yaad rakho
+Question	                Meaning
+Left rotate by d	        Left se d elements shift
+Right rotate by d	        Right se d elements shift
+d	                        Kitni positions rotate karna hai
+n	                        Array ki length
+d % n	                    d ko valid range me lana
+Left rotation               split	0...d-1 and d...n-1
+
+
+Que. arr = [1, 2, 3, 4, 5]
+     d = 2
+
+1. Left rotate by 2 ka matlab:
+1 2 | 3 4 5
+↑ ↑
+
+Pehle 2 elements left side se bahar jayenge:
+3 4 5 | 1 2
+
+Final: [3, 4, 5, 1, 2]
+
+
+2. Right rotate by 2:
+Original:
+1 2 3 | 4 5
+        ↑ ↑
+        last 2 elements
+
+Last 2 elements ko beginning me le aao:
+4 5 | 1 2 3
+
+Final: [4, 5, 1, 2, 3]
+*/
