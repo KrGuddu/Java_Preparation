3e43ece2b21e793a0d71e129d3a53e49bb090b01
KrGuddu <krguddu684@gmail.com>
2026-09-15 21:55:44 +0530

    refactor: Updates logics of missing number

--- src/Array/MissingNumber.java
+++ src/Array/MissingNumber.java
@@ -8,13 +8,23 @@ public class MissingNumber {
         int[] arr = {1,2,3,5};
         System.out.println(missingNum(arr));
     }
-    public static int missingNum(int arr[]) {
+    public static int missingNum(int[] arr) {
         int n = arr.length + 1;     // in question given size is n - 1      //Note: agar array ki length 7 hogi to 8 element hoge, matlab 1 add ho jayege.
-        int sum = n*(n+1)/2;
-        int arrSum = 0;
+        int sum = n*(n+1)/2;        //Expected Sum
+        int arrSum = 0;             //Actual Sum
         for(int ele : arr){
             arrSum += ele;
         }
         return sum - arrSum;
     }
 }
+
+/* A/C gfg que missing array length is n-1 in which one element is missing, So actual array length is arr.length + 1   ==>>jo ek missing element tha usko add kar diya.
+
+In this question original array length is 5 but one element is missing
+missing number = Expected Sum - Actual Sum
+               =(1+2+3+4+5) - (1+2+3+5)
+               = 15 - 11
+               =4
+So, missing number is 4.
+ */
