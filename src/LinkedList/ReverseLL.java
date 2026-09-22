8baad351b6f1de3fcd59ee4eabee134de72be38e
KrGuddu <krguddu684@gmail.com>
2026-09-22 20:46:08 +0530

    Refactor: Update 2nd method of reverse of a linked list problem

--- src/LinkedList/ReverseLL.java
+++ src/LinkedList/ReverseLL.java
@@ -14,19 +14,31 @@ class Node {
 public class ReverseLL {
     Node reverseList(Node head) {
         //1st method : Using Extra Array(ArrayList Method): T.C.= O(n), A.S.= O(n)
-         Node temp = head;
-         ArrayList<Node> arr = new ArrayList<>();
-         while(temp!=null){
-             arr.add(temp);
-             temp = temp.next;
-         }
+//         Node temp = head;
+//         ArrayList<Node> arr = new ArrayList<>();
+//         while(temp!=null){
+//             arr.add(temp);
+//             temp = temp.next;
+//         }
+//
+//         int n = arr.size();
+//         for(int i=n-1;i>=1;i--){
+//             arr.get(i).next = arr.get(i-1);
+//         }
+//         arr.get(0).next = null;
+//         return arr.get(n-1);
 
-         int n = arr.size();
-         for(int i=n-1;i>=1;i--){
-             arr.get(i).next = arr.get(i-1);
-         }
-         arr.get(0).next = null;
-         return arr.get(n-1);
+        //2nd method: 3-pointer approach(Iterative way):  T.C.= O(n), A.S.= O(1)       ==>Best for interview
+        Node curr = head;
+        Node prev = null;
+        Node fwd = null;            //We can set fwd on null, or head or anywhere on the list.
+        while(curr != null){
+            fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+        }
+        return prev;
 
     }
     public static void main(String[] args) {
