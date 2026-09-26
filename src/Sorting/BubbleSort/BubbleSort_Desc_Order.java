7b7e59b9dffce6307b03811871ee584dfca0298c
KrGuddu <krguddu684@gmail.com>
2026-09-26 20:44:52 +0530

    refactor: update sorting codes

--- src/Sorting/BubbleSort/BubbleSort_Desc_Order.java
+++ src/Sorting/BubbleSort/BubbleSort_Desc_Order.java
@@ -8,7 +8,7 @@ public class BubbleSort_Desc_Order {
         for(int i=n-1;i>0;i++){ // n-1 passes
             int swaps = 0;
             for(int j=n-1;j>i;j++){
-                if(arr[j+1]>arr[j]){
+                if(arr[j+1]>arr[j]){        //also use this: arr[j]<arr[j+1]
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
